70fc8c86ad5ac278aa9a6314133970f3d0225948
hokagelox <100351456+hokagelox@users.noreply.github.com>
2022-04-27 13:30:49 +0300

    Update os4.java

--- os4.java
+++ os4.java
@@ -1,16 +1,21 @@
-public class Main {
+public class Main
+{
+
     public static void main(String args[]) {
-        int a=0;
-        int b=3;
-        int c=3;
-        int i1=100000000;
-        long m = System.currentTimeMillis();
-        for (int i = 0; i < i1; i++){
-            a = a+(b*2 + c - i);
+
+        long t = System.currentTimeMillis();
+        long a = 0;
+        int b = 3;
+        int c = 3;
+        int i1 = 100000000;
+
+        for (int i = 1; i <= i1; i++)
+        {
+            a += (b * 2 + c - i);
         }
 
-
+        System.out.println("Время выполнения программы: " + (System.currentTimeMillis() - t) + "миллисекунд");
         System.out.println("Результат работы: " + a);
-        System.out.println("Время выполнения программы: " + (System.currentTimeMillis() - m));
     }
+
 }
